fd9fcc17e20b694b035c42233f185f0a3cde9c7a
sahilchanna <78895649+sahilchanna@users.noreply.github.com>
2021-03-22 12:33:43 +0530

    Update help.py

--- Zect/modules/help.py
+++ Zect/modules/help.py
@@ -5,16 +5,16 @@ from Zect.helpers.pyrohelper import get_arg
 
 HELP.update(
     {
-        "**Admin Tools**": "__ban, unban, promote, demote, kick, mute, unmute, gmute, ungmute, pin, purge, del, invite__",
-        "**AFK**": "__afk, unafk__",
-        "**Alive**": "__alive, ping__",
-        "**Developer**": "__eval, term__",
-        "**Misc**": "__paste, tr, whois, id__",
-        "**Notes**": "__save, get, clear, clearall, notes__",
-        "**Anti-PM**": "__pmguard, setpmmsg, setlimit, setblockmsg, allow, deny__",
-        "**Sticker**": "__kang, stkrinfo__",
-        "**Greetings**": "__setwelcome, clearwelcome__",
-        "**Updater**": "__update__",
+        "**✴️Admin Tools**": "__ban, unban, promote, demote, kick, mute, unmute, gmute, ungmute, pin, purge, del, invite__",
+        "**✴️AFK**": "__afk, unafk__",
+        "**✴️Alive**": "__alive, ping__",
+        "**✴️Developer**": "__eval, term__",
+        "**✴️Misc**": "__paste, tr, whois, id__",
+        "**✴️Notes**": "__save, get, clear, clearall, notes__",
+        "**✴️Anti-PM**": "__pmguard, setpmmsg, setlimit, setblockmsg, allow, deny__",
+        "**✴️Sticker**": "__kang, stkrinfo__",
+        "**✴️Greetings**": "__setwelcome, clearwelcome__",
+        "**✴️Updater**": "__update__",
     }
 )
 
